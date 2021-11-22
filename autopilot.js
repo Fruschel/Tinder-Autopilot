46d3c0795c26edcca0cacff7b565dc18ebfe6631
Fruschel <kevma3@gmail.com>
2021-11-22 22:02:06 +0100

    Fixed code for current Tinder version

--- autopilot.js
+++ autopilot.js
@@ -1,9 +1,22 @@
 let isPaused = false;
 let isStopped = false;
+let matchDelay = 1500;
 const pads = new GamePads();
 
-function checkAvailableLikes() {
+function getElementByXpath(path) {
+    return document.evaluate(path, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+}
 
+function GamePads() {
+    let gamepad = getElementByXpath("/html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div");
+    this.subscription = document.getElementsByClassName('productButton__subscriptionButton');
+    this.passButton = gamepad.getElementsByTagName('button')[1];
+    this.superLikeButton = gamepad.getElementsByTagName('button')[2];
+    this.likeButton = gamepad.getElementsByTagName('button')[3];
+    this.counter = document.getElementsByClassName('Fz($ml)')[0];
+}
+
+function checkAvailableLikes() {
     if (pads.subscription.length > 0) {
         const countdownText = pads.counter.textContent;
         const countdownSplit = countdownText.split(':');
@@ -15,45 +28,11 @@ function checkAvailableLikes() {
     return 0;
 }
 
-function GamePads() {
-    let gamepad = document.getElementsByClassName('recsGamepad')[0];
-    this.subscription = document.getElementsByClassName('productButton__subscriptionButton');
-    this.passButton = gamepad.getElementsByTagName('button')[1];
-    this.superLikeButton = gamepad.getElementsByTagName('button')[2];
-    this.likeButton = gamepad.getElementsByTagName('button')[3];
-    this.counter = document.getElementsByClassName('Fz($ml)')[0];
-}
-
-function getRandomPeriod() {
-    return Math.floor(Math.random() * (5000 - 500 + 1)) + 500;
-}
-
-document.onkeydown = (ev) => {
-    switch (ev.key) {
-        case ' ':
-            isPaused = !isPaused;
-            console.log("Press Spacebar again to Pause/Continue");
-            break;
-        case 's':
-            console.log("STOPPED");
-            isStopped = true;
-            break;
-    }
-};
-
-function likeOrPass() {
-    let randomLiker = Math.floor(Math.random() * (12 - 1 + 1) + 1);
-    if (randomLiker > 10)
-        pads.passButton.click();
-    else
-        pads.likeButton.click();
-}
-
 function swipeRight() {
     if (isStopped)
         return;
     // A random period between 500ms and 2secs
-    let randomPeriod = getRandomPeriod();
+    let randomPeriod = matchDelay;
 
     setTimeout(() => {
         let isOutOfLikes = false;
@@ -69,7 +48,7 @@ function swipeRight() {
             if (isPaused) {
                 setTimeout(swipeRight, randomPeriod);
             } else {
-                likeOrPass();
+                pads.likeButton.click();
                 swipeRight();
             }
         } else {
@@ -77,6 +56,31 @@ function swipeRight() {
         }
     }, randomPeriod);
 }
+
+document.onkeydown = (ev) => {
+    switch (ev.key) {
+        case ' ':
+            isPaused = !isPaused;
+            console.log("Press Spacebar again to Pause/Continue");
+            break;
+        case 's':
+            console.log("STOPPED");
+            isStopped = true;
+            break;
+    }
+};
+
+// selector gamepad
+// #t-1646534024 > div > div.App__body.H\(100\%\).Pos\(r\).Z\(0\) > div > main > div.H\(100\%\) > div > div > div.recsCardboard.W\(100\%\).Mt\(a\).H\(100\%\)--s.Px\(4px\)--s.Pos\(r\) > div.recsCardboard__cardsContainer.H\(100\%\).Pos\(r\).Z\(1\) > div > div.Pos\(a\).B\(0\).Isolate.W\(100\%\).Start\(0\).End\(0\) > div
+
+// jspath gamepad
+// document.querySelector("#t-1646534024 > div > div.App__body.H\\(100\\%\\).Pos\\(r\\).Z\\(0\\) > div > main > div.H\\(100\\%\\) > div > div > div.recsCardboard.W\\(100\\%\\).Mt\\(a\\).H\\(100\\%\\)--s.Px\\(4px\\)--s.Pos\\(r\\) > div.recsCardboard__cardsContainer.H\\(100\\%\\).Pos\\(r\\).Z\\(1\\) > div > div.Pos\\(a\\).B\\(0\\).Isolate.W\\(100\\%\\).Start\\(0\\).End\\(0\\) > div")
+
+// xpath gamepad
+// //*[@id="t-1646534024"]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div
+
+// full xpath gamepad
+// /html/body/div[1]/div/div[1]/div/main/div[1]/div/div/div[1]/div[1]/div/div[4]/div
 
 console.log("PRESS SPACEBAR TO PAUSE/CONTINUE");
 console.log("PRESS 's' KEY TO STOP");
